1459e687f7e003c84585ed151e3002b287496520
Daniel Wang <PakourManiac2011@gmail.com>
2026-05-06 22:50:54 -0400

    Minor edits, change approach
    Reinforcement learning is not suitable after multiple revisions, new approach required

--- src/GD_env.py
+++ src/GD_env.py
@@ -8,7 +8,7 @@ import pyautogui
 # Screen reading
 import pytesseract
 import cv2
-from PIL import ImageGrab
+import mss
 
 # File pathing
 from pathlib import Path
@@ -23,6 +23,7 @@ secondsPerFrame = 1 / framesPerSecond
 screenWidth, screenHeight = pyautogui.size()
 centerX = screenWidth // 2
 centerY = screenHeight // 2
+percentageBox = {"left": 1640, "top": 185, "width": 230, "height": 60} # CHANGE TO MATCH PERCENTAGE BOX COORDINATES
 
 # Game environment
 class GameEnv(Env):
@@ -30,21 +31,25 @@ class GameEnv(Env):
         # Call parent initializer
         super().__init__()
 
-        # Actions: Nothing, tap, hold, release
-        self.action_space = Discrete(4)
+        # Actions: Nothing, tap
+        self.action_space = Discrete(2)
 
         # Observe current percentage
         self.observation_space = Box(
-            low = np.array([0.0, 0.0]),
-            high = np.array([100.0, 1.0]), 
-            dtype=np.float32
+            low = np.array([0.0]),
+            high = np.array([100.0]), 
+            dtype = np.float32
         )
+
+        # Create screenshot object
+        self.sct = mss.mss()
         
         # Set starting values
         self.percent = 0.0
         self.prev_percent = 0.0
-        self.best_percent = 00.0
+        self.best_percent = 0.0
 
+        self.frame_count = 0
         self.stuck_frames = 0
         self.max_stuck_frames = 500
 
@@ -55,53 +60,64 @@ class GameEnv(Env):
         if action == 0:
             pass
         elif action == 1:
-            pyautogui.click(x=centerX, y=centerY)
-        elif action == 2:
+            pyautogui.mouseDown(x = centerX, y = centerY)
+            time.sleep(0.01)
+            pyautogui.mouseUp(x = centerX, y = centerY)
+        
+        """ elif action == 2:
             if not self.holding:
                 pyautogui.mouseDown(x=centerX, y=centerY)
                 self.holding = True
         elif action == 3:
             if self.holding:
                 pyautogui.mouseUp(x=centerX, y=centerY)
-                self.holding = False
+                self.holding = False """
         
         # Wait one frame
         time.sleep(secondsPerFrame)
 
         # Read new percentage
         self.prev_percent = self.percent
-        self.percent = self.get_percentage()
+        self.frame_count += 1
+        read_percent = False
+
+        if self.frame_count % 3 == 0:
+            self.percent = self.get_percentage()
+            read_percent = True
 
         # Calculate progress made
         raw_progress = self.percent - self.prev_percent
 
-        # Check if the bot has died
-        if (raw_progress < -0.1):
+        reward = 0.0
+        terminated = False
 
-            reward = -10
+        # Check if the bot has died
+        if raw_progress < -0.1 and read_percent:
+            reward -= 30
             terminated = True
 
         # Calculate reward otherwise
         else:
             progress = max(0.0, raw_progress)
-            reward = progress * 10
 
-            # Reward for new best
-            if (self.percent > self.best_percent):
-                self.best_percent = self.percent
-                reward += 1
-            
-            # Check if bot is stuck or dead
-            if raw_progress <= 0.0001:
-                self.stuck_frames += 1
-            else:
-                self.stuck_frames = 0
+            if read_percent:
+                reward += progress
+                
+                # Update best percent
+                if self.percent > self.best_percent:
+                    self.best_percent = self.percent
+                
+                # Check if bot is stuck or dead
+                if raw_progress <= 0.0001:
+                    self.stuck_frames += 1
+                else:
+                    self.stuck_frames = 0
 
             if self.percent >= 100:
                 reward += 100
                 terminated = True
             elif self.stuck_frames >= self.max_stuck_frames:
-                reward -= 10
+                reward -= 30
                 terminated = True
             else:
                 terminated = False
@@ -109,8 +125,10 @@ class GameEnv(Env):
         
         # Set return values
         truncated = False
-        obs = np.array([self.percent, float(self.holding)], dtype=np.float32)
-        info = {"percent": self.percent, "best_percent": self.best_percent}
+        obs = np.array([self.percent], dtype=np.float32)
+        info = {"percent": self.percent, 
+                "best_percent": self.best_percent, 
+            }
 
         return obs, reward, terminated, truncated, info
 
@@ -125,17 +143,18 @@ class GameEnv(Env):
             pyautogui.mouseUp(x=centerX, y=centerY)
             self.holding = False
 
-        # Restart level with 2 frame buffer
+        # Restart level with buffer
         pyautogui.press('r')
-        time.sleep(secondsPerFrame * 2)
+        time.sleep(0.05)
 
         # Read starting percentage
         self.percent = self.get_percentage()
         self.prev_percent = self.percent
+        self.frame_count = 0
         self.stuck_frames = 0
 
         # Set return values
-        obs = np.array([self.percent, float(self.holding)], dtype=np.float32)
+        obs = np.array([self.percent], dtype=np.float32)
         info = {}
 
         return obs, info
@@ -146,17 +165,16 @@ class GameEnv(Env):
         pytesseract.pytesseract.tesseract_cmd = str(filePath.absolute())
         
         # Reads the percentage as a string
-        img = ImageGrab.grab(bbox = (1640, 185, 1870, 245)) # NEEDS TO MATCH PERCENTAGE BOX COORDINATES
+        img = np.array(self.sct.grab(percentageBox))
         inp = pytesseract.image_to_string(
-            cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY), 
-            lang = 'eng'
-        ) 
+            cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY),
+            lang = "eng",
+            config = "--psm 7 -c tessedit_char_whitelist=0123456789.%"
+        )
 
         # Clean up the string
         inp = inp.strip().replace('%', '').replace('O', '0').replace('o', '0')
-        for char in inp:
-            if not char.isdigit() and char != '.':
-                inp = inp.replace(char, '')
+        inp = "".join(char for char in inp if char.isdigit() or char == ".")
 
         # Convert string percent to float
         try: percent = float(inp)
@@ -164,8 +182,19 @@ class GameEnv(Env):
 
         return percent
     
-    def close(self):
-        # Stops holding if the bot is still holding down when the environment is closed
-        if self.holding:
-            pyautogui.mouseUp(x=centerX, y=centerY)
-            self.holding = False
+    """ def record_death_percent(self, percent):
+        self.death_percents.append(percent)
+
+        if len(self.death_percents) > 20:
+            self.death_percents.pop(0)
+
+        if len(self.death_percents) >= 5:
+            avg_death_percent = sum(self.death_percents) / len(self.death_percents)
+            close_deaths = 0
+
+            for p in self.death_percents:
+                if abs(p - avg_death_percent) < 0.2:
+                    close_deaths += 1
+            
+            if close_deaths >= 3:
+                self.stuck_death_percent = avg_death_percent """
--- src/train.py
+++ src/train.py
@@ -5,16 +5,33 @@ from GD_env import GameEnv
 # File managing
 import os
 
+# Input checking
+import keyboard
+
 # Create folders for models and logs
 os.makedirs("models", exist_ok = True)
 os.makedirs("logs", exist_ok = True)
 
+# Allow for manual start
+while True:
+    if keyboard.is_pressed('z'): # CHANGE 'z' TO ANY KEY YOU WANT AS START
+        break
+
 # Initialize environment and model
 env = GameEnv()
-model = PPO("MlpPolicy", env, verbose = 1, tensorboard_log = "./logs/")
+env.adapt_mode = True
+model = PPO(
+    "MlpPolicy", 
+    env, 
+    verbose = 1, 
+    tensorboard_log = "./logs/", 
+    n_steps = 128, 
+    batch_size = 64,
+    ent_coef = 0.1
+)
 
 # Start training
-model.learn(total_timesteps = 10000)
+model.learn(total_timesteps = 5000)
 
 # Save trained model and close environment
 model.save("models/gd_ppo")
